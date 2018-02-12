754b7e3972d02a7fe4a6f679f076093162f115e3
Amit Kumar <amit.kumar@telegraph.co.uk>
2018-02-12 21:52:49 +0000

    minor nitpick setup.py of c_extension

--- python_c_extension/setup.py
+++ python_c_extension/setup.py
@@ -1,9 +1,9 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('extension_module',
-                    sources = ['python_c_extension/extension_module.c'])
+ext_module = Extension('extension_module',
+                       sources=['python_c_extension/extension_module.c'])
 
-setup (name = 'Extension Module',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
+setup(name='Extension Module',
+      version='1.0',
+      description='Python C Extension module',
+      ext_modules=[ext_module])
