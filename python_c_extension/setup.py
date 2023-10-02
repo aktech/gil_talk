4bc997dd6b03153e7feb29645c74fa00e502eb0c
Amit Kumar <dtu.amit@gmail.com>
2023-10-02 01:14:32 +0100

    use setuptools instead

--- python_c_extension/setup.py
+++ python_c_extension/setup.py
@@ -1,9 +1,13 @@
-from distutils.core import setup, Extension
+from setuptools import Extension, setup
 
-ext_module = Extension('extension_module',
-                       sources=['python_c_extension/extension_module.c'])
-
-setup(name='Extension Module',
-      version='1.0',
-      description='Python C Extension module',
-      ext_modules=[ext_module])
+setup(
+    name='Extension Module',
+    version="0.2",
+    description='Python C Extension module',
+    ext_modules=[
+        Extension(
+            name="extension_module",
+            sources=["python_c_extension/extension_module.c"],
+        ),
+    ]
+)
